ceb191590de6d1fab11ba267aff6855e678ad1ef
joseph <doit@joseph.com>
2023-12-09 14:32:28 +0800

    test Profile annotation.

--- 1-basic/dubbo-samples-spring-boot/dubbo-samples-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/TestProfile.java
+++ 1-basic/dubbo-samples-spring-boot/dubbo-samples-spring-boot-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/TestProfile.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile({ "!test & !dev" })
+//@Profile({ "!test & !dev" })
+//@Profile({"!test", "!dev"})   // dev 执行
+@Profile({ "!stress & !dev" })  // stress 不执行
 @Component
 public class TestProfile {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestProfile.class);
